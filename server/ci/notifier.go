2c9a178ca7fd803adec06f3195915b037350b6db
Zou Rui <21751189@zju.edu.cn>
2018-06-18 19:57:27 +0800

    monday work
    Signed-off-by: Zou Rui <21751189@zju.edu.cn>

--- server/ci/notifier.go
+++ server/ci/notifier.go
@@ -46,7 +46,7 @@ func (n *Notifier) TravisCIProcess(input string) error {
 	logrus.Info(input)
 	var tw TravisWebhook
 	if err := json.Unmarshal([]byte(input), &tw); err != nil {
-		return err
+		return fmt.Errorf("unmarsal error: %v", err)
 	}
 
 	prNum := tw.PullRequestNumber
@@ -86,6 +86,7 @@ func (n *Notifier) TravisCIProcess(input string) error {
 
 // CircleCIProcess gets the json string and acts to these messages from CircleCI.
 func (n *Notifier) CircleCIProcess(input string) error {
+	logrus.Infof("here is input :%v", input)
 	input = strings.Replace(input, `\"`, `"`, -1)
 	logrus.Info(input)
 	var cw CircleCIWebhook
