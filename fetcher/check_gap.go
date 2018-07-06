e1e21a20b57f4296f0c911b2f07438c2836a39f9
Zou Rui <21751189@zju.edu.cn>
2018-07-06 15:23:25 +0800

    Revert "fix stuck problem when detecting gaps"

--- fetcher/check_gap.go
+++ fetcher/check_gap.go
@@ -38,12 +38,6 @@ func (f *Fetcher) CheckPRsGap() error {
 		return err
 	}
 
-	if err := updateMasterBr(); err != nil {
-		logrus.Errorf("failed to prepare master env: %v", err)
-		return err
-	}
-	logrus.Infof("prepare master env done")
-
 	for _, pr := range prs {
 		logrus.Info("start to check prs")
 		go func(pr *github.PullRequest) {
@@ -62,14 +56,24 @@ func (f *Fetcher) checkPRGap(p *github.PullRequest) error {
 		return err
 	}
 
+	// get master branch info
+	if err := prepareMasterEnv(); err != nil {
+		return err
+	}
+	logrus.Infof("prepare master env done :pr %d", *(p.Number))
+
 	msLogString, err := getLogInfo("master")
 	if err != nil {
 		return err
 	}
-	logrus.Infof("get master br log done :pr %d", *(p.Number))
+	logrus.Infof("get log info done :pr %d", *(p.Number))
 
 	// get pr branch info
 	prNum := strconv.Itoa(*p.Number)
+
+	// FIXME handle the situation when failed to clean existing pr branch.
+	cleanPrBranchEnv(prNum)
+	logrus.Infof("clean exsiting branch done:pr %d", *(p.Number))
 
 	if err = preparePrBranchEnv(prNum); err != nil {
 		return err
@@ -155,7 +159,7 @@ func (f *Fetcher) AddGapCommentToPR(pr *github.PullRequest, gap int) error {
 	return f.client.AddCommentToIssue(*(pr.Number), newComment)
 }
 
-func updateMasterBr() error {
+func prepareMasterEnv() error {
 	cmd := exec.Command("git", "checkout", "master")
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("failed to checkout master: %v", err)
@@ -180,12 +184,26 @@ func preparePrBranchEnv(prNum string) error {
 		return fmt.Errorf("failed to pull pr %s: %v", prNum, err)
 	}
 
+	cmd = exec.Command("git", "checkout", "new-"+prNum)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to checkout pr %s branch: %v", prNum, err)
+	}
+
+	return nil
+}
+
+func cleanPrBranchEnv(prNum string) error {
+	cmd := exec.Command("git", "branch", "-D", "new-"+prNum)
+	// TODO ignore not found error
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to remove existing pr %s branch %v", prNum, err)
+	}
 	return nil
 }
 
 func getLogInfo(branch string) (string, error) {
 	var Out bytes.Buffer
-	cmd := exec.Command("git", "log", branch, "--oneline")
+	cmd := exec.Command("git", "log", "--oneline")
 	cmd.Stdout = &Out
 	if err := cmd.Run(); err != nil {
 		return "", fmt.Errorf("failed to get %s log: %v", branch, err)
